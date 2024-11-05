4fda06335c6752afa553fa90ceca31d9f51a225f
Igor Kobelev <kia1809@yandex.ru>
2024-11-05 23:42:43 +0300

    the form is almost ready

--- 9_html_and_css/course_paper/src/js/feedback.js
+++ 9_html_and_css/course_paper/src/js/feedback.js
@@ -4,9 +4,11 @@ const onFeedbackForm = document.getElementById('button-mail');
 const offFeedbackForm = document.getElementById('feedback-button');
 
 onFeedbackForm.addEventListener('click', () => {
+    console.log('открыли');
     document.querySelector('.forms').classList.remove('remove-element');
 });
 
 offFeedbackForm.addEventListener('click', () => {
+    console.log('скрыли');
     document.querySelector('.forms').classList.add('remove-element');
 });
