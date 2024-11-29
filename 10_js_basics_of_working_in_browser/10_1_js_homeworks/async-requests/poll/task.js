8f6843293f20cc0eab94738e140b08754a712f85
Igor Kobelev <kia1809@yandex.ru>
2024-11-29 18:15:33 +0300

    poll task is completed

--- 10_js_basics_of_working_in_browser/10_1_js_homeworks/async-requests/poll/task.js
+++ 10_js_basics_of_working_in_browser/10_1_js_homeworks/async-requests/poll/task.js
@@ -2,6 +2,7 @@ const answerTitle = document.querySelector('#poll__title');
 const answersBox = document.querySelector('#poll__answers');
 
 let answersId = null;
+let answersArray = null;
 
 
 const xhrAnswers = new XMLHttpRequest;
@@ -10,21 +11,31 @@ xhrAnswers.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
 const xhrStatistics = new XMLHttpRequest;
 xhrStatistics.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll');
 xhrStatistics.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-// xhrStatistics.send('vote=1&answer=2');
+
 
 xhrAnswers.send();
 xhrAnswers.onload = () => {
   const responseData = JSON.parse(xhrAnswers.response);
   answersId = responseData.id;
   answerTitle.textContent = responseData.data.title;
-  createButtonAnswers(responseData.data.answers);
+  answersArray = responseData.data.answers;
+  createButtonAnswers(answersArray);
+};
+
+xhrStatistics.onload = () => {
+  const responseData = JSON.parse(xhrStatistics.response);
+  showStatistics(responseData.stat);
 };
 
 
 answersBox.addEventListener('click', (e) => {
   if (e.target.classList.contains('poll__answer')) {
     alert('Спасибо, ваш голос засчитан!')
-    console.log(e.target.textContent);
+    const answerIndex = answersArray.findIndex(
+      (elem) => elem === e.target.textContent
+    );
+    xhrStatistics.send(`vote=${answersId}&answer=${answerIndex}`);
+    document.querySelector('.poll__answers').style.display = 'none';
   }
 });
 
@@ -37,5 +48,15 @@ function createButtonAnswers(answers) {
   });
 }
 
-
-
+function showStatistics(stat) {
+  const statBlock = document.createElement('div');
+  const allVotes = stat.reduce((a, b) => {return a + b.votes}, 0);
+  stat.forEach((elem) => {
+    statBlock.insertAdjacentHTML('beforeend', `
+      <div>
+        ${elem.answer}: <b>${(elem.votes / allVotes * 100).toFixed(2)}%</b>
+      </div>
+      `)
+  });
+  document.querySelector('.poll').insertAdjacentElement('beforeend', statBlock);
+}
